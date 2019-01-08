9fbda2968cfb4c5db261b4c62410e5396a8c7ca6
Ido Barkan <ido@appsflyer.com>
2019-01-08 13:34:18 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerospike-clj "0.1.0-SNAPSHOT"
+(defproject aerospike-clj "0.1.0"
   :description "An Aerospike Clojure client."
   :url "https://github.com/AppsFlyer/aerospike-clj"
   :license {:name "Eclipse Public License"
