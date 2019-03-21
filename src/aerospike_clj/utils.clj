7d8fb03c36361227259897f8c5907de05e1ee475
Dhruvil Patel <dhruvil.patel@kirasystems.com>
2019-03-20 22:37:51 -0400

    Utils - adding transducers, predicate for string keys

--- src/aerospike_clj/utils.clj
+++ src/aerospike_clj/utils.clj
@@ -18,28 +18,39 @@
   not accepted. The conversion happens only at the top-level of the bin and does not
   seem to affect nested structures. To store these values in the database, they are
   converted to keywords."
-  {:true  true
-   :false false
-   :nil   nil})
+  {true  :true
+   false :false
+   nil   :nil})
 
+;; transducers
+(def ^:private x-sanitize
+  (replace boolean-replacements))
+
+(def ^:private x-desanitize
+  (replace (set/map-invert boolean-replacements)))
+
+;; predicates
 (defn single-bin?
   "Predicate function to determine whether data will be stored as a single bin or
   multiple bin record."
   [bin-names]
   (= bin-names [""]))
 
+(defn string-keys?
+  "Predicate function to determine whether all keys provided for bins are strings."
+  [bin-names]
+  (every? string? bin-names))
+
 (defn sanitize-bin-value
   "Values in nested structures are unaffected and do not need to be sanitized. If,
   however, `true`, `false` or `nil` exist as the only value in a bin, they need to
   be sanitized."
   [bin-value]
-  (->> (vector bin-value)
-       (replace (set/map-invert boolean-replacements))
+  (->> (into [] x-sanitize (vector bin-value))
        first))
 
 (defn desanitize-bin-value
   "Converts sanitized (keywordized) bin values back to their original value."
   [bin-value]
-  (->> (vector bin-value)
-       (replace boolean-replacements)
+  (->> (into [] x-desanitize (vector bin-value))
        first))
