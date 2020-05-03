91d0b091f329de089e5e233341dc037872881a9f
Ido Barkan <ido@appsflyer.com>
2020-05-03 15:21:34 +0300

    fix NPE for null sets

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerospike-clj "0.5.1"
+(defproject aerospike-clj "0.5.2"
   :description "An Aerospike Clojure client."
   :url "https://github.com/AppsFlyer/aerospike-clj"
   :license {:name "Eclipse Public License"
