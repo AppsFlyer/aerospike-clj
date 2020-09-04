763dc86cb5d8fd26237d52dfe614dafdfd646d3a
Preda Radu <predar@playtika.com>
2020-09-04 18:19:11 +0300

    Remove unused setter calls in map->event-policy Add tests

---
+++ test/aerospike_clj/policy_test.clj
@@ -0,0 +1,30 @@
+(ns aerospike-clj.policy-test
+  (:require [clojure.test :refer [deftest is]]
+            [aerospike-clj.policy :as policy])
+  (:import [com.aerospike.client.async EventPolicy]))
+
+(defn- verify-event-policy-properties
+  [^EventPolicy event-policy conf]
+  (is (= (get conf "maxCommandsInProcess" 0) (.-maxCommandsInProcess event-policy)))
+  (is (= (get conf "maxCommandsInQueue" 0) (.-maxCommandsInQueue event-policy)))
+  (is (= (get conf "queueInitialCapacity" 256) (.-queueInitialCapacity event-policy)))
+  (is (= (get conf "minTimeout" 100) (.-minTimeout event-policy)))
+  (is (= (get conf "ticksPerWheel" 256) (.-ticksPerWheel event-policy)))
+  (is (= (get conf "commandsPerEventLoop" 256) (.-commandsPerEventLoop event-policy))))
+
+(deftest get-valid-event-policy-with-default-properties
+  (let [conf {}
+        event-policy (policy/map->event-policy conf)]
+    (verify-event-policy-properties event-policy conf)))
+
+(deftest get-valid-event-policy-with-max-commands-process-and-queue
+  (let [conf {"maxCommandsInProcess" 100 "maxCommandsInQueue" 1}
+        event-policy (policy/map->event-policy conf)]
+    (verify-event-policy-properties event-policy conf)))
+
+(deftest invalid-event-policy-exception
+  (let [conf {"maxCommandsInProcess" 100 "maxCommandsInQueue" 0}]
+    (is (thrown-with-msg?
+          Exception
+          #"setting maxCommandsInProcess>0 and maxCommandsInQueue=0 creates an unbounded delay queue"
+          (policy/map->event-policy conf)))))
