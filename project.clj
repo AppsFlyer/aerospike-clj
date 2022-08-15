7713723c4c1285de6a9e665caf78b8487e939c89
chorin1 <16934179+chorin1@users.noreply.github.com>
2022-08-15 16:27:28 +0300

    2.0.6 snapshot [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.appsflyer/aerospike-clj "2.0.5-SNAPSHOT"
+(defproject com.appsflyer/aerospike-clj "2.0.6-SNAPSHOT"
   :description "An Aerospike Clojure client."
   :url "https://github.com/AppsFlyer/aerospike-clj"
   :license {:name "Eclipse Public License"
