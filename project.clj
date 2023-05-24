43c752c1d8ab95d77bfecf49dae492f7a7ae60a0
Asaf Chelouche <asaf.chelouche@appsflyer.com>
2023-05-24 16:58:26 +0300

    2.0.8-SNAPSHOT [skip-ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.appsflyer/aerospike-clj "2.0.7"
+(defproject com.appsflyer/aerospike-clj "2.0.8-SNAPSHOT"
   :description "An Aerospike Clojure client."
   :url "https://github.com/AppsFlyer/aerospike-clj"
   :license {:name "Eclipse Public License"
